39e8d112c57e7896dd28da40a33cc23b7330e35d
ecln30 <ecln30@gmail.com>
2022-05-09 10:42:06 +0900

    finish calculator

--- main.js
+++ main.js
@@ -1,55 +1,66 @@
 const buttons = document.querySelectorAll(".button")
 const input = document.querySelector(".input")
 const del = document.querySelector(".del")
+const container = document.querySelector(".container")
+const button = document.querySelector("button")
+const ball = document.querySelector(".ball")
 
-let preValue 
+
+let firstValue 
 let newValue 
 let operator 
 let operation = false
+let calculate = false
 let resultDisplayed = false
-
 buttons.forEach(item => {
     item.addEventListener("click", display)
 })
 
 function display(e) {
-     const el = e.target
+     e.stopPropagation
+     const el = e.currentTarget
      const value = el.dataset.id
      const ops = el.dataset.ops
      const currentValue = input.textContent
 
      if (operation === false && resultDisplayed === false || currentValue === "0") {
-         input.textContent = currentValue.replace("0", "")
-         input.textContent +=  value
+               
+         if(value == ops) value = ""
+         if( currentValue.length <= 14 ) {
+             
+            input.textContent = currentValue.replace("0", "")
+            + value
+            console.log(currentValue)
+         } 
          
      } else if ( operation === true) {
-         
-          input.textContent = "" 
-          input.textContent = value
-          operation = false
-         
+              input.textContent = ""
+              input.textContent =  value
+              operation = false
+
      } else if (resultDisplayed == true) {
-          
-          input.textContent = ""
-          input.textContent = value
-          resultDisplayed = false
+              input.textContent = ""
+              input.textContent = value
+              resultDisplayed = false
      } 
 
      if ( ops === "+" || ops === "-" || ops === "x" || ops === "/") {
          
          input.textContent = currentValue.replace(value, "")
-         preValue = parseFloat(currentValue)
+         firstValue = parseFloat(currentValue)
          operation = true
          operator = ops
     }
 
-    if ( ops === "equal" || el.matches("span")) {
-             
+    if ( ops === "equal" || el.matches("span") && operator !== "") {
+        console.log(ops)
+       
         newValue = parseFloat(input.textContent.replace("value", ""))
-        Evaluate(preValue, newValue, operator)
+        Evaluate(firstValue, newValue, operator)
+    
     }
     else if ( ops === "del"){
-         input.textContent = currentValue.replace("del", "")
+         
          input.textContent = currentValue.slice(0, -1)
          if( input.textContent.length <= 0) {
              input.textContent = "0"
@@ -57,11 +68,14 @@ function display(e) {
     }
     else if( ops === "reset" ) {
           input.textContent = "0"
+          preValue = ""
+          newValue = ""
     }
 }
 
 function Evaluate(a, b, c) {
-
+      
+     if(isNaN(a) || isNaN(b))return
      if( c === "+") {
          input.textContent = a + b
      }
@@ -75,18 +89,12 @@ function Evaluate(a, b, c) {
          input.textContent = a * b
      }
       resultDisplayed = true
+      firstValue = ""
+      newValue = ""
+      operation = flase
 }
 
-
-
-
-
-
-
-
-
-
-
+// change Theme
 
 
 
